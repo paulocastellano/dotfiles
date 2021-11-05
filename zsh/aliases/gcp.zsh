0af9c1b52ff078ccbd24c6670ead33c562b6cad0
Paulo Castellano <paulofcastellano@gmail.com>
2021-11-05 11:59:18 -0300

    remove some unused alias and add laravel vapor

--- zsh/aliases/gcp.zsh
+++
@@ -1,4 +0,0 @@
-# Google Cloud Platform
-
-# Open Tunnel Development Enviroment
-alias gcp_open_tunnel_mysql_dev='gcloud compute ssh redis-mysql-staging --ssh-flag=" -fN -L 33069:127.0.0.1:3306"'
--- zsh/exports/kubernetes.zsh
+++
@@ -1,2 +0,0 @@
-# KOPS production
-export KOPS_STATE_STORE=s3://k8s.1591630269.changelogfy.com
